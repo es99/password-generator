349f9f628d8b763099c9321cc120e0b24ffc6811
Engels Souza <engels.franca@icloud.com>
2024-09-10 19:45:02 -0300

    atualizando algumas coisas da aplicacao para poder conteneiriza-la

---
+++ environment.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/sh
+
+export FLASK_APP=generatorpass/app.py
+export FLASK_ENV=production
+
+sleep 1
+
+echo "done!"
--- generatorpass/ext/db/models.py
+++ generatorpass/ext/db/models.py
@@ -11,3 +11,15 @@ class Senhas(db.Model):
 
     def __repr__(self):
         return "%r. <%r>" % (self.id, self.user_email)
+    
+class Servidores(db.Model):
+    __tablename__ = "servidores"
+    id = db.Column("id", db.Integer, primary_key=True)
+    servidor = db.Column("server", db.String(50))
+    ip = db.Column("ip", db.String(50))
+    user = db.Column("user", db.String(50))
+    password = db.Column("password", db.Unicode)
+    data_raw = db.Column("data_cadastro", db.DateTime)
+    
+    def __repr__(self):
+        return "%r - %r" % (self.ip, self.servidor)
